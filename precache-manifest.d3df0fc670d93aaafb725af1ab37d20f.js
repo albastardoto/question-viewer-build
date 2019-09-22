42f0d1bc996ea56ba4c8531502a6f0ed83475947
albastardoto <albert.koppelmaa@hotmail.com>
2019-09-22 16:15:28 +0200

    working build hopefully

---
+++ precache-manifest.d3df0fc670d93aaafb725af1ab37d20f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c15af6e3db2e90142d179c8c1ff8fd6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fce696a4a1889cdc1eb0",
+    "url": "/static/css/main.822270b8.chunk.css"
+  },
+  {
+    "revision": "1308025a7b5f9d585dad",
+    "url": "/static/js/2.50288b06.chunk.js"
+  },
+  {
+    "revision": "fce696a4a1889cdc1eb0",
+    "url": "/static/js/main.56004640.chunk.js"
+  },
+  {
+    "revision": "e3ae7e0b27f7f22adfbe",
+    "url": "/static/js/runtime~main.39e88118.js"
+  }
+]);
